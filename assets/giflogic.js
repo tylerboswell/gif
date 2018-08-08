b146e8386a7644dd965973ce87ce366ea3807405
tpb1109 <tyler.boswell@interdynartis.com>
2018-08-08 09:46:36 -0400

    comment out console log

--- assets/giflogic.js
+++ assets/giflogic.js
@@ -1,3 +1,4 @@
+//Initial movie buttons
 var MovieGif = {
     btnArray : ["princess bride", "gladiator", "kingsmen", "wolf of wallstreet", "deadpool"],
     selected : "",
@@ -15,28 +16,32 @@ var MovieGif = {
 
 MovieGif.popBtns();
 
+//display gifs on button click
 $('#buttons-area').on('click', '.btn', function(){
     $('#images-area').empty();
-    console.log($(this).attr('data-value'));
+    // console.log($(this).attr('data-value'));
     var selected = $(this).attr('data-value')
+
+ //URL to get GIFs   
 $.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?api_key=iiSvA3Jf2Uwi2AvE36ypagPMCAQWWu9N&q="+ selected +"&limit=10&offset=0&rating=PG&lang=en",
+    url: "https://api.giphy.com/v1/gifs/search?api_key=iiSvA3Jf2Uwi2AvE36ypagPMCAQWWu9N&q="+ selected +"&limit=10&offset=0&lang=en",
     method: "GET",
     
+    //Display gifs
 }).then(function (response) {
-    console.log(response);
-    for (var c = 0; c < response.data.length; c++) {
+    // console.log(response);
+    for (var i = 0; i < response.data.length; i++) {
         var newCard = $('<div>');
         newCard.attr('class', 'card')
         .attr('style','float:left; width: 18rem;');
 
         var cardImg = $('<img>');
         cardImg.attr('class','gif')
-        .attr('src', response.data[c].images.original_still.url)
-        .attr('alt', response.data[c].title)
+        .attr('src', response.data[i].images.original_still.url)
+        .attr('alt', response.data[i].title)
         .attr('data-state','still')
-        .attr('data-still', response.data[c].images.original_still.url)
-        .attr('data-animate', response.data[c].images.original.url)
+        .attr('data-still', response.data[i].images.original_still.url)
+        .attr('data-animate', response.data[i].images.original.url)
         .attr('style','height:200px');
 
         var cardBody= $('<div>');
@@ -44,7 +49,7 @@ $.ajax({
         .attr('style', 'height:100px');
 
         var ratingDiv = $('<h5>');
-        var rating = response.data[c].rating;
+        var rating = response.data[i].rating;
         ratingDiv.attr('class','card-title')
         .attr('id','rating')
         .attr('class', 'text-capitalize')
@@ -59,6 +64,7 @@ $.ajax({
 })
 });
 
+//play gifs when you click the images
 $("#images-area").on("click", '.gif' , function() {
       var state = $(this).attr("data-state");
       if (state === "still") {
@@ -69,7 +75,7 @@ $("#images-area").on("click", '.gif' , function() {
         $(this).attr("data-state", "still");
       }
     });
-
+//add movie to list
 $('#addMovie').on('click', function(){
     var newShow = $('#input-movie').val().trim().toLowerCase()
     MovieGif.btnArray.push(newShow);
